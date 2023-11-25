4ad892ebce34ffa6d4e3463e6905897b317f84e2
ippfcox <765472938@qq.com>
2023-11-25 17:39:24 +0800

    change Tab behavior, change menu style

--- lua/plugin-configs/nvim-cmp.lua
+++ lua/plugin-configs/nvim-cmp.lua
@@ -1,5 +1,4 @@
 local cmp = require('cmp')
-local lspkind = require('lspkind')
 
 cmp.setup({
     snippet = {
@@ -7,7 +6,13 @@ cmp.setup({
             vim.fn["vsnip#anonymous"](args.body)
         end,
     },
-    -- window = {},
+    window = {
+        completion = {
+            winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,Search:None',
+            col_offset = -3,
+            side_padding = 0,
+        },
+    },
     mapping = require('keybindings').cmp(cmp),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
@@ -18,14 +23,14 @@ cmp.setup({
         { name = 'path' },
     }),
     formatting = {
-        format = lspkind.cmp_format({
-            with_text = true,
-            maxwidth = 50,
-            before = function(entry, vim_item)
-                vim_item.menu = '['..string.upper(entry.source.name)..'['
-                return vim_item
-            end
-        })
+        fields = { 'kind', 'abbr', 'menu' },
+        format = function(entry, vim_item)
+            local kind = require('lspkind').cmp_format({ mode = 'symbol_text', maxwidth = 50 })(entry, vim_item)
+            local strings = vim.split(kind.kind, '%s', { trimempty = true })
+            kind.kind = ' ' .. (strings[1] or '') .. ' '
+            kind.menu = '[' .. (strings[2] or '') .. ']'
+            return kind
+        end,
     },
 })
 
