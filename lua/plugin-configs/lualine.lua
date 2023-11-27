990e75c4b4857dd3e2cd7f9f85a61c1c472d450c
ippfcox <765472938@qq.com>
2023-11-27 22:27:36 +0800

    config sepatartors

--- lua/plugin-configs/lualine.lua
+++ lua/plugin-configs/lualine.lua
@@ -2,15 +2,15 @@ require("lualine").setup({
 	options = {
 		icons_enabled = true,
 		theme = "auto",
-		component_separators = {},
-		section_separators = {},
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
 			statusline = {},
 			winbar = {},
 		},
 		ignore_focus = {},
 		always_divide_middle = true,
-		globalstatus = false,
+		globalstatus = true,
 		refresh = {
 			statusline = 1000,
 			tabline = 1000,
