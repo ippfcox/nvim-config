950f6f47eb0105b6793b84f8734d1678b88ffd98
ippfcox <765472938@qq.com>
2023-12-17 22:14:41 +0800

    change style, bold function name

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,5 +1,14 @@
-local colorscheme = "tokyonight"
-require(colorscheme).setup({})
+require("tokyonight").setup({
+  style = "moon",
+  styles = {
+    comments = { italic = true },
+    keywords = { italic = true },
+    functions = { bold = true },
+    variables = {},
+    sidebars = "dark",
+    floats = "dark",
+  },
+})
 vim.o.background = "dark"
 vim.o.termguicolors = true
 vim.cmd.colorscheme("tokyonight-moon")
