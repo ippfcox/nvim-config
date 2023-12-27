6f6980e3d0178c7c3fd242b135e8a5e24aa0696f
ippfcox <765472938@qq.com>
2023-12-27 19:47:59 +0800

    add mouse hover support via hover.nvim

--- lua/plugin-configs/pc-hover.lua
+++ lua/plugin-configs/pc-hover.lua
@@ -19,22 +19,16 @@ M.config = {
         },
         -- Whether the contents of a currently open hover window should be moved
         -- to a :h preview-window when pressing the hover keymap.
-        preview_window = false,
+        preview_window = true,
         title = true,
         mouse_providers = {
           "LSP",
         },
-        mouse_delay = 1000,
+        mouse_delay = 300,
       })
-    end,
 
-    -- Setup keymaps
-    -- vim.keymap.set("n", "K", require("hover").hover, {desc = "hover.nvim"})
-    -- vim.keymap.set("n", "gK", require("hover").hover_select, {desc = "hover.nvim (select)"})
-    --
-    -- -- Mouse support
-    -- vim.keymap.set('n', '<MouseMove>', require('hover').hover_mouse, { desc = "hover.nvim (mouse)" })
-    -- vim.o.mousemoveevent = true
+      vim.keymap.set({ "", "i" }, "<MouseMove>", require("hover").hover_mouse, { desc = "hover.nvim (mouse)" })
+    end,
   },
 }
 
